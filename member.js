dd18d94a1ea4f9cbba8e37ed88e953034854fad0
shivamj1008 <151640349+shivamj1008@users.noreply.github.com>
2023-11-21 14:46:45 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,9 +1,3 @@
 function skillMember() {
-    var member = document.getElementById("member");
-    var memberValue = member.options[member.selectedIndex].value;
-    if (memberValue == "member") {
-        document.getElementById("memberSkill").style.display = "block";
-    } else {
-        document.getElementById("memberSkill").style.display = "none";
-    }
+    console.log('skillMember');
 }
