7f75bf1e9843d19e6acd20d562c634bb4d7172d7
shivamj1008 <151640349+shivamj1008@users.noreply.github.com>
2023-11-21 15:27:22 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,70 +1,28 @@
-//Create web server
-const express = require("express");
-const router = express.Router();
-const Comment = require("../models/Comment");
-const auth = require("../middleware/auth");
-const Post = require("../models/Post");
-
-// @route   POST comment/create
-// @desc    Create a comment
-// @access  Private
-router.post("/create", auth, async (req, res) => {
-  try {
-    const { postId, comment } = req.body;
-
-    //Validation
-    if (!postId || !comment) {
-      return res.status(400).json({ msg: "Not all fields have been entered" });
-    }
-
-    const post = await Post.findById(postId);
-    if (!post) {
-      return res.status(400).json({ msg: "Post does not exist" });
-    }
-
-    const newComment = new Comment({
-      comment,
-      post,
-      postedBy: req.user.id,
-    });
-
-    const savedComment = await newComment.save();
-    res.json(savedComment);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+//create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+//set port
+var port = process.env.PORT || 8080;
+//use body parser to get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+//create router
+var router = express.Router();
+//middleware to use for all requests
+router.use(function(req, res, next){
+	//do logging
+	console.log('Something is happening.');
+	next(); //make sure we go to the next routes and don't stop here
 });
-
-// @route   GET comment/:postId
-// @desc    Get all comments for a post
-// @access  Private
-router.get("/:postId", auth, async (req, res) => {
-  try {
-    const comments = await Comment.find({ post: req.params.postId }).populate(
-      "postedBy",
-      "-password"
-    );
-    res.json(comments);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+//test route to make sure everything is working
+router.get('/', function(req, res){
+	res.json({message: 'hooray! welcome to our api!'});
 });
-
-// @route   GET comment/:commentId
-// @desc    Get a comment
-// @access  Private
-router.get("/:commentId", auth, async (req, res) => {
-  try {
-    const comment = await Comment.findById(req.params.commentId).populate(
-      "postedBy",
-      "-password"
-    );
-    res.json(comment);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// @route   PUT comment/:commentId
-// @desc    Update a comment
-// @access  Private
+//more routes for our API will happen here
+//register our routes
+//all of our routes will be prefixed with /api
+app.use('/api', router);
+//start server
+app.listen(port);
+console.log('Magic happens on port ' + port);
