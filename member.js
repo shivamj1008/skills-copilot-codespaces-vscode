4bc01d1c742587f171b8eb3cd76fbfa4618b61a9
shivamj1008 <151640349+shivamj1008@users.noreply.github.com>
2023-11-21 14:51:34 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,9 @@
-function skillMember() {
-    console.log("skillMember");
+function skillsMember() {
+  const skills = document.querySelectorAll('.skill');
+  const skillContainer = document.querySelector('.skills-container');
+  const skillContainerHeight = skillContainer.clientHeight;
+
+  skills.forEach((skill) => {
+    skill.style.height = `${skillContainerHeight}px`;
+  });
 }
